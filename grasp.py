8873c16d90b8491605776819c17bf9e6dffa63e4
Juan Sebastián Díaz Osorio <jsdiazo@eafit.edu.co>
2023-03-20 16:34:02 -0500

    feat: GRASP improved

--- grasp.py
+++ grasp.py
@@ -7,10 +7,9 @@ def getDistance(n1, n2):
   x2, y2 = n2
   return np.sqrt((x2-x1)**2 + (y2-y1)**2)
 
-def run(n, R, Q, Th, alpha, data):
-  start = time.time()
-
-  # Initialize data
+def initialize(n, R, Q, data):
+  availables = np.full(R, Q)
+  total_distances = np.zeros(R)
   distances = np.zeros((n + 1, n + 1))
   demands = np.zeros(n + 1)
   for node in data:
@@ -18,40 +17,49 @@ def run(n, R, Q, Th, alpha, data):
     distances[node[0]] = values
     demands[node[0]] = node[3]
 
-  # Initialize variables
-  paths = {i: [0] for i in range(R)}
-  availables = np.full(R, Q)
-  total_distances = np.zeros(R)
+  return [distances, demands, availables, total_distances]
 
+def run(n, R, Q, Th, alpha, data):
+  start = time.time()
+
+  # Initialize data
+  distances, demands, availables, total_distances = initialize(n, R, Q, data)
+  paths = {i: [0] for i in range(R)}
   i = 0
+  error = False
 
   while sum(demands) > 0:
-    actual_node = paths[i][-1]
-    candidates = {}
-    for j in range(n+1):
-      if 0 < demands[j] and demands[j] <= availables[i]:
-        candidates[j] = distances[actual_node][j]
+    candidates = []
+    for i in range(R):
+      actual_node = paths[i][-1]
+      some_trip = False
+      for j in range(n+1):
+        if 0 < demands[j] and demands[j] <= availables[i]:
+          some_trip = True
+          candidates.append([i, j, distances[actual_node][j]])
+
+      if not some_trip:
+        total_distances[i] += distances[actual_node][0]
+        availables[i] = Q
+        paths[i].append(0)
 
     if len(candidates) == 0:
-      total_distances[i] += distances[actual_node][0]
-      availables[i] = Q
-      paths[i].append(0)
-      #i = (i + 1) % R
+      if error: raise Exception('No solution found!')
+      error = True
       continue
 
-    c_min = min(candidates.values())
-    c_max = max(candidates.values())
+    _, _, c_min = min(candidates, key=lambda x: x[2])
+    _, _, c_max = max(candidates, key=lambda x: x[2])
 
     upper_bound = c_min + alpha * (c_max - c_min)
-    rcl = list(filter(lambda x: distances[actual_node][x] <= upper_bound, candidates))
-    next_node = random.choice(rcl)
+    rcl = list(filter(lambda x: x[2] <= upper_bound, candidates))
+    actual_truck, next_node, _ = random.choice(rcl)
+    actual_node = paths[actual_truck][-1]
 
-    total_distances[i] += distances[actual_node][next_node]
-    availables[i] -= demands[next_node]
+    total_distances[actual_truck] += distances[actual_node][next_node]
+    availables[actual_truck] -= demands[next_node]
     demands[next_node] = 0
-    paths[i].append(next_node)
-
-    i = (i + 1) % R
+    paths[actual_truck].append(next_node)
 
   for actual_truck in range(R):
     actual_node = paths[actual_truck][-1]
